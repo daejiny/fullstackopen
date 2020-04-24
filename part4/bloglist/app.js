cc80b071db8243eafff3615eaf29598b70de4035
Dae Jin Yuk <daejiny@gmail.com>
2020-04-24 01:57:50 -0700

    separate index and app, add middleware

---
+++ part4/bloglist/app.js
@@ -0,0 +1,27 @@
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+logger.info('Connecting to MongoDB')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  }).catch((e) => {
+    logger.error('Failed to connect to MongoDB:', e.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+
+module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,19 +1,10 @@
+const app = require('./app')
 const http = require('http')
-const logger = require('./utils/logger')
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
